72c56d4e506a1b008bf3395d3a569de54fb5e7d1
Iraldo <igonzalezc@iesfbmoll.org>
2019-05-19 22:42:44 +0200

    Segundo Commit - Ajustes + HistoriaCasal + Boton Idioma
    Añadidos comentarios en el CSS;
    Ajuste en el orden de los componentes dentro de "tronco";
    Eliminada propiedad "max-width" del menú lateral (no tenía ningún sentido, width basta);
    Introducido texto de "Historia del Casal" de prueba;
    Añadido texto de Historia casal de prueba;
    Añadida firma de autor y doctype;
    Añadido botón para cambiar de Idioma;
    Añadida firma de autor

--- code/javascript/scripts.js
+++ code/javascript/scripts.js
@@ -1,1 +1,5 @@
-jQuery("a").fitText(0.5);
+/* CÓDIGO POR: IRALDO ARMANDO GONZÁLEZ CARVALLO
+   ESTUDIANTE DE GRADO SUPERIOR - IES FRANCESC DE BORJAMOLL */
+function enviarFormLang() {
+    document.forms["formLang"].submit();
+}
